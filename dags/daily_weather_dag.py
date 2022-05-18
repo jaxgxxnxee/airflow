69aa6b856aaf9354878cfff968617e8bceec3d02
nsa18685 <nsa18685@gmail.com>
2022-05-18 21:48:24 +0900

    feedback 반영

--- dags/daily_weather_dag.py
+++ dags/daily_weather_dag.py
@@ -55,18 +55,17 @@ def load(**context):
     cur = get_Redshift_connection()
     daily_weather_info = context["task_instance"].xcom_pull(
         key="return_value", task_ids="transform")
-    daily_weather_info = iter(daily_weather_info)
-    next(daily_weather_info)
+    # 20220518: feedback
     sql = "CREATE TABLE {schema}.temp_{table} AS SELECT * FROM {schema}.{table};"
+    for drow in daily_weather_info:
+        if drow != "":
+            sql += f"""INSERT INTO {schema}.temp_{table} (date, temp, min_temp, max_temp) VALUES ('{drow['date']}', '{drow['temp']}', '{drow['min_temp']}', '{drow['max_temp']}');"""
+
     sql += "BEGIN; DELETE FROM {schema}.{table};".format(
         schema=schema, table=table)
     sql += """INSERT INTO {schema}.{table}
                SELECT date, temp, min_temp, max_temp, created_date 
                 FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY date ORDER BY created_date DESC) seq FROM {schema}.temp_{table}) WHERE seq = 1"""
-    for drow in daily_weather_info:
-        if drow != "":
-            sql += f"""INSERT INTO {schema}.{table} (date, temp, min_temp, max_temp) VALUES ('{drow['date']}', '{drow['temp']}', '{drow['min_temp']}', '{drow['max_temp']}');"""
-    sql += "END;"
     logging.info(sql)
     cur.execute(sql)
 
