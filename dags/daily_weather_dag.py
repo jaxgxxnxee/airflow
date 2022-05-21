c6e27a8593ce20a1ea2efb28a8e3f13c56c12474
nsa18685 <nsa18685@gmail.com>
2022-05-21 10:13:27 +0900

    ctas defaults...해결

--- dags/daily_weather_dag.py
+++ dags/daily_weather_dag.py
@@ -55,34 +55,22 @@ def load(**context):
     cur = get_Redshift_connection()
     daily_weather_info = context["task_instance"].xcom_pull(
         key="return_value", task_ids="transform")
-    # 20220518: feedback
-    # sql = f"""CREATE TABLE {schema}.temp_{table}
-    #          AS SELECT * FROM {schema}.{table};"""
-    # for drow in daily_weather_info:
-    #     if drow != "":
-    #         sql += f"""INSERT INTO {schema}.temp_{table} (date, temp, min_temp, max_temp) VALUES ('{drow['date']}', '{drow['temp']}', '{drow['min_temp']}', '{drow['max_temp']}');"""
-
-    # sql += "BEGIN; DELETE FROM {schema}.{table};".format(
-    #     schema=schema, table=table)
-    # sql += f"""INSERT INTO {schema}.{table}
-    #            SELECT date, temp, min_temp, max_temp, created_date
-    #             FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY date ORDER BY created_date DESC) seq FROM {schema}.temp_{table}) WHERE seq = 1; """
-    # sql += """END;"""
-
-    # euni: create_date를 살리기 위해
+    # 20220518, 20220521: feedback
     # redshift에서: CREATE TABLE AS(CTAS) 테이블은 자신이 생성된 테이블로부터 제약 조건, 자격 증명 열, 기본 열 값 또는 기본 키를 상속하지 않습니다.
-    # row_number() 잘못이해함..
-    sql = "BEGIN;"
+    sql = f"""CREATE TABLE {schema}.temp_{table}
+             (Like SELECT * FROM {schema}.{table} INCLUDING DEFAULTS);"""
+    sql = f"""INSERT INTO {schema}.temp_{table} SELECT * FROM {schema}.{table};"""
     for drow in daily_weather_info:
         if drow != "":
-            sql += f"""INSERT INTO {schema}.{table} (date, temp, min_temp, max_temp) VALUES ('{drow['date']}', '{drow['temp']}', '{drow['min_temp']}', '{drow['max_temp']}');"""
+            sql += f"""INSERT INTO {schema}.temp_{table} (date, temp, min_temp, max_temp) VALUES ('{drow['date']}', '{drow['temp']}', '{drow['min_temp']}', '{drow['max_temp']}');"""
 
-    sql += f"""CREATE TABLE {schema}.temp_{table} AS SELECT date, temp, min_temp, max_temp, created_date 
-                FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY date ORDER BY created_date DESC) seq FROM {schema}.{table}) WHERE seq = 1;"""
-    sql += f"DELETE FROM {schema}.{table};"
-    sql += f"""INSERT INTO {schema}.{table} SELECT * FROM {schema}.temp_{table};"""
-    sql += f"""DROP TABLE IF EXISTS {schema}.temp_{table};"""
+    sql += "BEGIN; DELETE FROM {schema}.{table};".format(
+        schema=schema, table=table)
+    sql += f"""INSERT INTO {schema}.{table} 
+               SELECT date, temp, min_temp, max_temp, created_date 
+                FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY date ORDER BY created_date DESC) seq FROM {schema}.temp_{table}) WHERE seq = 1; """
     sql += """END;"""
+
     logging.info(sql)
     cur.execute(sql)
 
